78fe397624c01cbd1815b705bf8af8a8530e148e
Jan Dietrich <jan.dietrich@sft-basel.ch>
2017-04-05 11:38:14 +0200

    initial presentation

--- src/main.js
+++ src/main.js
@@ -5,6 +5,20 @@ import "openlayers/css/ol.css!"
 
 hljs.initHighlighting();
 
+let map = new ol.Map({
+    target: "map",
+    layers: [
+        new ol.layer.Tile({
+            source: new ol.source.OSM()
+        }),
+        clusterLayer
+    ],
+    view: new ol.View({
+        center: [115817734252.48119, 7614510.238026799],
+        zoom: 7
+    })
+});
+
 // d3.csv("renewable_power_plants_DK.csv", function(error, data) {
     // if (error) throw error;
     // let filteredData = data.filter( row => (row.long!=="" && row.lat!==""))
@@ -46,49 +60,37 @@ let clusterSource = new ol.source.Cluster({
 let styleCache={};
 let clusterLayer = new ol.layer.Vector({
     source: clusterSource,
-    style: style
-    // style: function(feature) {
-    //     var size = feature.get('features').length;
-    //     var style = styleCache[size];
-    //     if (!style) {
-    //         style = new ol.style.Style({
-    //             image: new ol.style.Circle({
-    //                 radius: 10,
-    //                 stroke: new ol.style.Stroke({
-    //                     color: '#fff'
-    //                 }),
-    //                 fill: new ol.style.Fill({
-    //                     color: '#3399CC'
-    //                 })
-    //             }),
-    //             text: new ol.style.Text({
-    //                 text: size.toString(),
-    //                 fill: new ol.style.Fill({
-    //                     color: '#fff'
-    //                 })
-    //             })
-    //         });
-    //         styleCache[size] = style;
-    //     }
-    //     return style;
-    // }
+    // style: style
+    style: function(feature) {
+        var size = feature.get('features').length;
+        var style = styleCache[size];
+        if (!style) {
+            style = new ol.style.Style({
+                image: new ol.style.Circle({
+                    radius: 10,
+                    stroke: new ol.style.Stroke({
+                        color: '#fff'
+                    }),
+                    fill: new ol.style.Fill({
+                        color: '#3399CC'
+                    })
+                }),
+                text: new ol.style.Text({
+                    text: size.toString(),
+                    fill: new ol.style.Fill({
+                        color: '#fff'
+                    })
+                })
+            });
+            styleCache[size] = style;
+        }
+        return style;
+    }
 });
 
 
-let map = new ol.Map({
-    target: "map",
-    layers: [
-        new ol.layer.Tile({
-            source: new ol.source.OSM()
-        }),
-        clusterLayer
-    ],
-    view: new ol.View({
-        center: [115817734252.48119, 7614510.238026799],
-        zoom: 7
-    })
-});
 
-debugger;
+map.on('click', event => console.log(event.coordinate));
+
 
 // })
