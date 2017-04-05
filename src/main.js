d9abc2f4c80e54e99b82338f073ffb16e2388844
Jan Dietrich <jan.dietrich@sft-basel.ch>
2017-04-05 19:42:03 +0200

    presentation now

--- src/main.js
+++ src/main.js
@@ -5,28 +5,32 @@ import "openlayers/css/ol.css!"
 
 hljs.initHighlighting();
 
+let viewCenter = [828176.4324327, 5934540.317516159];
+let denmarkCenter = [1054310.920350848,7586500.221287746]
+
+let view = new ol.View({});
+let osmLayer = new ol.layer.Tile({
+    source: new ol.source.OSM()
+});
+
 let map = new ol.Map({
     target: "map",
     layers: [
-        new ol.layer.Tile({
-            source: new ol.source.OSM()
-        }),
-        clusterLayer
+        osmLayer
     ],
-    view: new ol.View({
-        center: [115817734252.48119, 7614510.238026799],
-        zoom: 7
-    })
+    view: view
 });
 
-// d3.csv("renewable_power_plants_DK.csv", function(error, data) {
-    // if (error) throw error;
-    // let filteredData = data.filter( row => (row.long!=="" && row.lat!==""))
-    //
-    // let features = filteredData.map( (row,index,all) => new ol.Feature({
-    //     name: "name"+index,
-    //     geometry: new ol.geom.Point(ol.proj.transform([Number(row.lon), Number(row.lat)], "EPSG:4326", "EPSG:3857"))
-    // }))
+let mapEl = document.getElementById("map");
+
+console.log()
+
+let style2 = new ol.style.Style({
+    image: new ol.style.Circle({
+        radius: 7,
+        stroke: new ol.style.Stroke({color: "blue", width: 6})
+    })
+});
 
 let stroke = new ol.style.Stroke({color: "blue", width: 3})
 let image = new ol.style.Circle({
@@ -37,9 +41,21 @@ let style = new ol.style.Style({
     image: image
 });
 
-// let originalSource = new ol.source.Vector({
-//     features: features
-// })
+
+let simplePoint = new ol.Feature({
+    geometry: new ol.geom.Point(viewCenter)
+});
+
+let simplePointSource = new ol.source.Vector({
+    features: [simplePoint]
+})
+
+
+let simplePointLayer = new ol.layer.Vector({
+    style: style2,
+    source: simplePointSource
+})
+
 let format = new ol.format.GeoJSON();
 let url = 'renewable_power_plants_DK.geojson';
 let originalSource = new ol.source.Vector({
@@ -92,5 +108,39 @@ let clusterLayer = new ol.layer.Vector({
 
 map.on('click', event => console.log(event.coordinate));
 
+document.getElementById("osm").addEventListener("impress:stepenter", function() {
+    if (mapEl.classList.contains("hidden")) {
+        mapEl.classList.remove("hidden")
+    }
+    view.setCenter([0, 0]);
+    view.setZoom(4);
+})
+document.getElementById("changeView").addEventListener("impress:stepenter", function() {
+    setTimeout( function() {
+        view.setCenter(viewCenter);
+        view.setZoom(18);
+    }, 2000)
+})
+document.getElementById("addPoint").addEventListener("impress:stepenter", function() {
+    setTimeout( function() {
+        view.setCenter(viewCenter);
+        view.setZoom(18);
+        map.addLayer(simplePointLayer);
+    }, 2000)
+})
+document.getElementById("addPoint").addEventListener("impress:stepleave", function() {
+    // map.removeLayer(simplePointLayer);
+    map.removeLayer(map.getLayerGroup[1]);
+})
 
-// })
+document.getElementById("addData").addEventListener("impress:stepenter", function() {
+    view.setCenter(denmarkCenter);
+    view.setZoom(7);
+    map.addLayer(clusterLayer);
+})
+
+document.getElementById("addData").addEventListener("impress:stepleave", function() {
+    map.removeLayer(map.getLayerGroup[1]);
+    mapEl.classList.add("hidden");
+
+})
